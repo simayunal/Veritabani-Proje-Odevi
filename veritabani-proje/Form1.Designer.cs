956a0034c93bac690435580190b42b9181edd21b
simayunal <104105012+simayunal@users.noreply.github.com>
2022-12-30 11:37:35 +0300

    Veritabani-Proje-Odevi
    veritabanini c# ile baglama

---
+++ veritabani-proje/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace veritabani_proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnlistele = new System.Windows.Forms.Button();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.btnguncelle = new System.Windows.Forms.Button();
+            this.txtno = new System.Windows.Forms.TextBox();
+            this.txtyazaradi = new System.Windows.Forms.TextBox();
+            this.txttarih = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NO = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(873, 261);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnlistele
+            // 
+            this.btnlistele.Location = new System.Drawing.Point(21, 354);
+            this.btnlistele.Name = "btnlistele";
+            this.btnlistele.Size = new System.Drawing.Size(189, 68);
+            this.btnlistele.TabIndex = 1;
+            this.btnlistele.Text = "Listele";
+            this.btnlistele.UseVisualStyleBackColor = true;
+            this.btnlistele.Click += new System.EventHandler(this.btnlistele_Click);
+            // 
+            // btnekle
+            // 
+            this.btnekle.Location = new System.Drawing.Point(237, 354);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(189, 68);
+            this.btnekle.TabIndex = 2;
+            this.btnekle.Text = "Ekle";
+            this.btnekle.UseVisualStyleBackColor = true;
+            this.btnekle.Click += new System.EventHandler(this.btnekle_Click);
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(1067, 41);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(179, 26);
+            this.txtid.TabIndex = 3;
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(1067, 164);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(179, 26);
+            this.txtad.TabIndex = 4;
+            // 
+            // btnsil
+            // 
+            this.btnsil.Location = new System.Drawing.Point(460, 354);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(189, 68);
+            this.btnsil.TabIndex = 5;
+            this.btnsil.Text = "Sil";
+            this.btnsil.UseVisualStyleBackColor = true;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // btnguncelle
+            // 
+            this.btnguncelle.Location = new System.Drawing.Point(696, 354);
+            this.btnguncelle.Name = "btnguncelle";
+            this.btnguncelle.Size = new System.Drawing.Size(189, 68);
+            this.btnguncelle.TabIndex = 6;
+            this.btnguncelle.Text = "Güncelle";
+            this.btnguncelle.UseVisualStyleBackColor = true;
+            this.btnguncelle.Click += new System.EventHandler(this.btnguncelle_Click);
+            // 
+            // txtno
+            // 
+            this.txtno.Location = new System.Drawing.Point(1067, 106);
+            this.txtno.Name = "txtno";
+            this.txtno.Size = new System.Drawing.Size(179, 26);
+            this.txtno.TabIndex = 7;
+            // 
+            // txtyazaradi
+            // 
+            this.txtyazaradi.Location = new System.Drawing.Point(1067, 225);
+            this.txtyazaradi.Name = "txtyazaradi";
+            this.txtyazaradi.Size = new System.Drawing.Size(179, 26);
+            this.txtyazaradi.TabIndex = 8;
+            // 
+            // txttarih
+            // 
+            this.txttarih.Location = new System.Drawing.Point(1067, 282);
+            this.txttarih.Name = "txttarih";
+            this.txttarih.Size = new System.Drawing.Size(179, 26);
+            this.txttarih.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.Cursor;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1019, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ID";
+            // 
+            // NO
+            // 
+            this.NO.AutoSize = true;
+            this.NO.Location = new System.Drawing.Point(1013, 106);
+            this.NO.Name = "NO";
+            this.NO.Size = new System.Drawing.Size(32, 20);
+            this.NO.TabIndex = 13;
+            this.NO.Text = "NO";
+            this.NO.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(959, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "KİTAP ADI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(949, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 20);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "YAZAR ADI";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(920, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 20);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "TESLİM TARİHİ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1289, 531);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NO);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txttarih);
+            this.Controls.Add(this.txtyazaradi);
+            this.Controls.Add(this.txtno);
+            this.Controls.Add(this.btnguncelle);
+            this.Controls.Add(this.btnsil);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.btnekle);
+            this.Controls.Add(this.btnlistele);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnlistele;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.Button btnguncelle;
+        private System.Windows.Forms.TextBox txtno;
+        private System.Windows.Forms.TextBox txtyazaradi;
+        private System.Windows.Forms.TextBox txttarih;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label NO;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ veritabani-proje/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace veritabani_proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NpgsqlConnection baglanti = new NpgsqlConnection("server=localhost; port=5432; database=dbKutuphane; password=smymys140502; user id=postgres");
+
+        private void btnlistele_Click(object sender, EventArgs e)
+        {
+            string sorgu = "select * from kitaplar";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void btnekle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut1 = new NpgsqlCommand("insert into kitaplar (id,no,adi,yazaradi,teslimtarihi)" +
+                " values (@id,@no, @adi,@yazaradi, @teslimtarihi)", baglanti);
+            komut1.Parameters.AddWithValue("@id", int.Parse(txtid.Text));
+            komut1.Parameters.AddWithValue("@no", int.Parse(txtno.Text));
+            komut1.Parameters.AddWithValue("@adi", txtad.Text);
+            komut1.Parameters.AddWithValue("@yazaradi", txtyazaradi.Text);
+            komut1.Parameters.AddWithValue("@teslimtarihi", Convert.ToDateTime(txttarih.Text));
+            komut1.ExecuteNonQuery();
+            baglanti.Close();
+
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut2 = new NpgsqlCommand("delete from kitaplar where adi=@adi", baglanti);
+            komut2.Parameters.AddWithValue("@adi",txtad.Text);
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+        }
+
+        private void btnguncelle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut3 = new NpgsqlCommand("update kitaplar set teslimtarihi=@teslimtarihi where id=@id ", baglanti);
+            komut3.Parameters.AddWithValue("@id", int.Parse(txtid.Text));
+            komut3.Parameters.AddWithValue("@teslimtarihi", Convert.ToDateTime(txttarih.Text));
+            komut3.ExecuteNonQuery();
+            baglanti.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
